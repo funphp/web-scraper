d141e32fdaeb718f2734d6b9c2dc7d1d9a87d798
Abdullah <ab_duetcse@yahoo.com>
2017-04-18 01:17:18 +0600

    added webscraper to get author with articles

--- debug.php
+++ debug.php
@@ -1,5 +1,17 @@
 <?php
-  require 'src/webscraper.php';
-  $someParam = $argv[1];
-  placeholder($someParam);
+require 'vendor/autoload.php';
+use funphp\WebScraper;
+$longOpts  = array(
+    "startDate::",
+    "endDate::",
+    "concurrency::",
+    "maxResultsPerAuthor::",
+    "wait::",
+);
+$options = getopt(null, $longOpts);
+$scrapper = new WebScraper();
+foreach($options as $attribute => $value) {
+  $scrapper->$attribute = $value;
+}
+print_r($scrapper->parse());
 ?>
